07cf4a1c90a7fd56fac425e79d2e476fc342be05
sgoldsmith <sgoldsmith@dx4350>
2018-01-07 13:04:44 -0500

    Added schematic

--- python/ledtest.py
+++ python/ledtest.py
@@ -10,6 +10,7 @@ the LED), then the resistor to 3.3 V, and connect the cathode (the short
 pin) of the LED to pin 12 (IOG11). The anode of LED connects to a
 current-limiting resistor and then to 3.3V. Therefore, to turn on an LED,
 we need to make pin 12 low (0V) level. It can be realized by programming.
+See images/ledtest.jpg for schematic.
 
 Note: Only Swig wrapper is used here since no callbacks are used and there
 is no need for ctypes.
