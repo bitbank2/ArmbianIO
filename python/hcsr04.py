c58aa3ab1085404c6e2113cd79526a349f255ee3
sgoldsmith <sgoldsmith@dx4350>
2018-01-08 13:07:35 -0500

    Comment updates

--- python/hcsr04.py
+++ python/hcsr04.py
@@ -9,9 +9,13 @@ Distance will not be super accurate due to the nature of not using a realtime
 OS. This will not be an issue for a lot of applications. You will notice it
 works better bouncing sound off flat surfaces. Testing with people, etc. will
 yield some weird values. I used a NanoPi Duo for testing and wired echo to
-IRRX and trig to IOG11. You can use a moving average and toss out of bound
-values to get more accurate readings. Search around and you will find out
-how. This program uses very little CPU.
+pin 10 (IRRX) and trig to pin 12 (IOG11). You can use a moving average and toss
+out of bound values to get more accurate readings. Search around and you will
+find out how. This program uses very little CPU.
+
+Note: Stick with ctypes and do not mix armbianio wrapper module with ctypes
+since they are not interchangeable at runtime. I'm only using Swig wrapper
+for the constants.
 """
 
 import time
@@ -72,7 +76,7 @@ if rc == 1:
     armbianioLib.AIOAddGPIO(echo, GPIO_IN)
     # Callback prototype
     cfunc = CFUNCTYPE(None, c_int, c_int)
-    # Button callback
+    # Echo callback
     armbianioLib.AIOAddGPIOCallback(echo, EDGE_BOTH, cfunc(echoCallback));
     count = 0
     while count < 100:
